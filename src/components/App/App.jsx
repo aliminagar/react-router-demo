b1a6000a759bdc07a74b4515c309d40b0cb3a123
Ali Minagar <aminagar@gmail.com>
2025-03-13 22:43:03 -0500

    Initial commit: React Router implementation with navigation

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,16 +4,20 @@ import Header from "../Header/Header";
 import Dashboard from "../Dashboard/Dashboard";
 import Reviews from "../Reviews/Reviews";
 import AboutMe from "../AboutMe/AboutMe";
+import AboutUs from "../AboutUs/AboutUs";
+// Remove the NavBar import
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      {/* Remove the NavBar component from here */}
       <Routes>
         <Route path="/" element={<Dashboard />} />
         <Route path="/reviews" element={<Reviews />} />
         <Route path="/about-me" element={<AboutMe />} />
+        <Route path="/about-us" element={<AboutUs />} />
       </Routes>
     </div>
   );
